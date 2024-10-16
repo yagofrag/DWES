3e2d8e838276e6056fa52ad1367b6400bd869a01
yago fraguela moas <yfraguelam23@fpcoruna.afundacion.org>
2024-10-16 13:46:23 +0200

    tarea3

--- sprint1apache/www/site3/example3.php
+++ sprint1apache/www/site3/example3.php
@@ -2,10 +2,11 @@
 <body>
 <h1>Jubilación</h1>
 <?php
+$edad = isset($_GET['edad']) ? intval($_GET['edad']) : 0;
 function edad_en_10_años($edad) {
 return $edad + 10;
 }
-if (edad_en_10_años(58) > 65) {
+if (edad_en_10_años($edad) > 65) {
 echo "En 10 años tendrás edad de jubilación";
 } else {
 echo "¡Disfruta de tu tiempo!";
