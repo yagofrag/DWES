1070874c33938b660c743d5bd78069eaadb4e5c7
yago fraguela moas <yfraguelam23@fpcoruna.afundacion.org>
2024-10-16 12:55:12 +0200

    Tarea2

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>Página de bienvenidas</h1>
+<?php
+function dar_bienvenida($nombre) {
+echo "¡Bienvenido/a, " . $nombre . "!";
+}
+dar_bienvenida("Lisa Simpsons");
+?>
+</body>
+</html>
