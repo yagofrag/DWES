3ba4b134a0112f6c75b35cb347be6bcebfd1461d
yagofrag <yfraguelam23@fpcoruna.afundacion.org>
2025-01-20 21:19:24 +0100

    fase correcciones

--- Proyecto/PycharmProjects/TrabajoCarlos/eventos/models.py
+++ Proyecto/PycharmProjects/TrabajoCarlos/eventos/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class UsuarioPersonalizado(AbstractUser):
     ROL_CHOICES = [
         ('organizador', 'Organizador'),
@@ -13,14 +12,44 @@ class UsuarioPersonalizado(AbstractUser):
     def __str__(self):
         return f"{self.username} ({self.rol})"
 
-    class Evento(models.Model):
-        titulo = models.CharField(max_length=100)
-        descripcion = models.TextField()
-        fecha_hora = models.DateTimeField()
-        capacidad_maxima = models.PositiveIntegerField()
-        imagen_url = models.URLField(blank=True, null=True)
-        organizador = models.ForeignKey(UsuarioPersonalizado, on_delete=models.CASCADE,
-                                        limit_choices_to={'rol': 'organizador'})
 
-        def __str__(self):
-            return self.titulo
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_hora = models.DateTimeField()
+    capacidad_maxima = models.PositiveIntegerField()
+    imagen_url = models.URLField(blank=True, null=True)
+    organizador = models.ForeignKey(UsuarioPersonalizado, on_delete=models.CASCADE, limit_choices_to={'rol': 'organizador'})
+
+    def __str__(self):
+        return self.titulo
+
+
+
+
+class Reserva(models.Model):
+    ESTADO_CHOICES = [
+        ('pendiente', 'Pendiente'),
+        ('confirmada', 'Confirmada'),
+        ('cancelada', 'Cancelada'),
+    ]
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(UsuarioPersonalizado, on_delete=models.CASCADE, limit_choices_to={'rol': 'participante'})
+    numero_entradas = models.PositiveIntegerField()
+    estado = models.CharField(max_length=10, choices=ESTADO_CHOICES, default='pendiente')
+
+    def __str__(self):
+        return f"Reserva de {self.usuario.username} para {self.evento.titulo}"
+
+
+
+
+class Comentario(models.Model):
+    texto = models.TextField()
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(UsuarioPersonalizado, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.usuario.username} comenta en {self.evento.titulo}"
